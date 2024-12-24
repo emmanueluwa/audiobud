91a8a19b8e8c2c8c2d10a374eb9c4835b27b8a8a
emmanuel <oemmanuel.uwadiae@gmail.com>
2024-12-24 20:33:49 +0000

    feat: adding initial audiobud creation form

--- src/app/(root)/create-audiobud/page.tsx
+++ src/app/(root)/create-audiobud/page.tsx
@@ -1,11 +1,120 @@
-import React from "react";
+"use client";
 
-const CreateAudio = () => {
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+// import { toast } from "@/components/hooks/use-toast";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { cn } from "@/lib/utils";
+
+const FormSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+});
+
+const CreateAudiobud = () => {
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      username: "",
+    },
+  });
+
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    // toast({
+    //   title: "You submitted the following values:",
+    //   description: (
+    //     <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+    //       <code className="text-white">{JSON.stringify(data, null, 2)}</code>
+    //     </pre>
+    //   ),
+    // });
+  }
+
   return (
-    <div>
-      <h1 className="text-20 font-bold text-white-1">Create Audiobud</h1>
-    </div>
+    <section className="mt-10 flex flex-col">
+      <h1 className="text-20 font-bold text-white-1">Create Podcast</h1>
+
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="mt-12 flex w-full flex-col"
+        >
+          <div className="flex flex-col gap-[30px] border-b border-black-5 pb-10">
+            <FormField
+              control={form.control}
+              name="audiobudTitle"
+              render={({ field }) => (
+                <FormItem className="flex flex-col gap-2.5">
+                  <FormLabel className="text-16 font-bold text-white-1">
+                    Username
+                  </FormLabel>
+                  <FormControl>
+                    <Input
+                      className="input-class focus-visible:ring-red-1"
+                      placeholder="The meaning of life"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage className="text-white-1" />
+                </FormItem>
+              )}
+            />
+
+            <div className="flex flex-col gap-2.5">
+              <Label className="text-16 font-bold text-white-1">
+                Select Voice
+              </Label>
+
+              <Select>
+                <SelectTrigger
+                  className={cn(
+                    "text-16 w-full border-none bg-black-1 text-gray-1"
+                  )}
+                >
+                  <SelectValue
+                    placeholder="Select AI Voice"
+                    className="placeholder:text-gray-1"
+                  />
+                </SelectTrigger>
+                <SelectContent className="text-16 border-none bg-black-1 font-bold text-white-1 focus:ring-red-1">
+                  {["voice ein", "voice zwei"].map((option) => (
+                    <SelectItem
+                      key={option}
+                      value={option}
+                      className="capitalize focus:bg-red-1"
+                    >
+                      {option}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+            </div>
+          </div>
+        </form>
+      </Form>
+    </section>
   );
 };
 
-export default CreateAudio;
+export default CreateAudiobud;
--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -7,7 +7,7 @@ import { api } from "@/../convex/_generated/api";
 import { useQuery } from "convex/react";
 
 const Home = () => {
-  const tasks = useQuery(api.tasks.get);
+  // const tasks = useQuery(api.tasks.get);
 
   return (
     <div className="mt-9 flex flex-col gap-9">
@@ -15,7 +15,7 @@ const Home = () => {
         <h1 className="text-20 font-bold text-white-1">Trending Audiobuds</h1>
 
         <div className="flex min-h-screen flex-col items-center justify-between p-24 text-white-1">
-          {tasks?.map(({ _id, text }) => <div key={_id}>{text} hello</div>)}
+          {/* {tasks?.map(({ _id, text }) => <div key={_id}>{text} hello</div>)} */}
         </div>
 
         <div className="audio_grid">
